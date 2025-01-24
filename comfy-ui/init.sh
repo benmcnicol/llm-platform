bb9d87321457d97e0fd1a2538e9d618f65ee2ae1
Ben <benmcnicol@gmail.com>
2025-01-24 23:15:22 +1100

    deployment file

---
+++ comfy-ui/init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+if [ -e "/install.sh" ] ; then 
+	/install.sh
+	rm -rf /install.sh
+fi
+
+/usr/sbin/sshd
+/usr/sbin/nginx
+
+nohup filebrowser -d /opt/filebrowser/filebrowser.db -a 127.0.0.1 -p 81 -b /filebrowser -r /opt/ComfyUI/ --noauth > /dev/null &
+nohup ttyd --port 82 --base-path /ttyd /usr/bin/bash > /dev/null &
+
+cd /opt/ComfyUI
+
+echo "use $*"
+
+python3.11 main.py $*
